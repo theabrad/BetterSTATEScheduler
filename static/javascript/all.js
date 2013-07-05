d6231d2e503b03faa454a5c4ec4c1f7850c10ada
Alex Bradley <alex.guerrilla@gmail.com>
2013-07-04 20:04:17 -0400

    added date check

--- static/javascript/all.js
+++ static/javascript/all.js
@@ -11,9 +11,16 @@ $(document).ready(function () {
 
 	/*New Button */
 	$(function(){
-		//check the fall button
-		$('input:radio[name=toggle]')[0].checked = true;
-		$("#fall").addClass('select');
+		var month = new Date().getMonth();
+		if(month > 11) {
+			//check spring button
+			$('input:radio[name=toggle]')[1].checked = true;
+			$('#spring').addClass('select');
+		} else {
+		    //check the fall button
+		    $('input:radio[name=toggle]')[0].checked = true;
+		    $("#fall").addClass('select');
+		}
 
 		$(".b-style").click(function(){
 			$(".b-style").removeClass('select');
